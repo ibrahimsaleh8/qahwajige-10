ff8181e505d3fe98d315cf96f6a0987251ab3edb
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-07-31 12:43:57 +0300

    remove google verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,9 +67,6 @@ export async function generateMetadata(): Promise<Metadata> {
       alternates: {
         canonical: currentURL,
       },
-      verification: {
-        google: "Hqq8aaxAOSJDIOt44S3oopaS0l9bu1pHsr6s4WkROW0",
-      },
     };
   } catch (error) {
     console.error("Metadata fetch failed:", error);
